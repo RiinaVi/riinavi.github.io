0e3fde01505566a8abf468aaf13d5c5458cf0f1b
riinavi <riinavi86@gmail.com>
2023-04-12 00:04:42 +0300

    add theme to local storage

--- scripts/dark-theme.js
+++ scripts/dark-theme.js
@@ -1,9 +1,30 @@
-window.addEventListener("load", () => {
+const THEMES = {
+  dark: 'dark',
+  light: 'light',
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  const storedTheme = localStorage.getItem('theme');
+  
+  switch (storedTheme) {
+    case THEMES.dark:
+      document.body.classList.add(THEMES.dark);
+      return;
+    case THEMES.light:
+    default:
+      document.body.classList.remove(THEMES.dark);
+      return;
+  }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('btnSwitch').addEventListener('click',() => {
-    if (document.body.classList.contains('dark')) {
-      document.body.classList.remove('dark');
+    if (document.body.classList.contains(THEMES.dark)) {
+      document.body.classList.remove(THEMES.dark);
+      localStorage.setItem('theme', THEMES.light);
     } else {
-      document.body.classList.add('dark');
+      document.body.classList.add(THEMES.dark);
+      localStorage.setItem('theme', THEMES.dark);
     }
   });
 });
